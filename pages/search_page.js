54de06df529d5cdede4caa8060508d00c9944af9
Charles Bird <charliehbird@gmail.com>
2026-09-24 23:10:05 +0100

    Home page

---
+++ pages/search_page.js
@@ -0,0 +1,5 @@
+// Source - https://stackoverflow.com/a/4211930
+// Posted by alex, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-09-24, License - CC BY-SA 3.0
+document.getElementById('background').ondragstart = function() { return false; };
+// Disables dragging of the background image
